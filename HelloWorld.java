d11392021b38f7779ee97a4ccda805797dc8f47f
Ramona2226 <brisc.ramona22@yahoo.com>
2020-05-31 20:04:18 +0300

    added new line to demo collaborationg a project

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
 // comment added
 public static void main (String[] args) 
-System.out.println ("Hello World! We are learning VCS"); 
+System.out.println ("Hello World! We are learning VCS");
+System.out.println ("New line);
 }
 }
